8eb733a51775cdbc3e30e339a830575c62a3a361
Steven Dawson <steve@djdawson.co.uk>
2019-09-11 17:41:12 +0100

    Version bump for release

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("sjdawson.TruckSimulatorPlugin")]
-[assembly: AssemblyDescription("Additional properties for use in truck simulators, ETS and ATS.")]
+[assembly: AssemblyDescription("Additional properties for use in truck simulators, ETS2 and ATS.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("sjdawson")]
 [assembly: AssemblyProduct("sjdawson.TruckSimulatorPlugin")]
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("833040c9-fe5e-4ccf-b21d-71979e049b6b")]
 
-[assembly: AssemblyVersion("1.1.1")]
-[assembly: AssemblyFileVersion("1.1.1")]
+[assembly: AssemblyVersion("1.1.2")]
+[assembly: AssemblyFileVersion("1.1.2")]
